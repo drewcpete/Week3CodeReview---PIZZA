a806c086d9c1eed15f652c324b3c2f70d7f135c8
&rew Peterson <drewcpete@gmail.com>
2019-08-30 09:47:42 -0700

    hardcodes size/price into user interface.  will move into price prototype

--- js/scripts.js
+++ js/scripts.js
@@ -18,3 +18,21 @@ Pizza.prototype.price = function () {
     this.pizza.text("22")
   }
 };
+
+/////////User Interface//////////
+$(function(){
+  var pizza = new Pizza();
+  // pizza.price(size),
+  $("input:radio[value=12-inch]").click(function(){
+    $("priceOut").text("small, 12-inch pizza")
+  })
+  $("input:radio[value=16-inch]").click(function() {
+    $("priceOut").text("medium, 16-inch pizza")
+  })
+  $("input:radio[value=20-inch]").click(function() {
+    $("priceOut").text("large, 20-inch pizza")
+  })
+
+
+
+});
