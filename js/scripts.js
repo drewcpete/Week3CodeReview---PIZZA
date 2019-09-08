61fcee9b37281fed9ad8ab3a2e70a24390850972
andrew <drewcpete@gmail.com>
2019-09-08 12:51:44 -0700

    fix topping being put into html li elements

--- js/scripts.js
+++ js/scripts.js
@@ -7,23 +7,26 @@ function Pizza(size) {
 
 Pizza.prototype.findTotal = function() {
   if (this.size === "12") {
-    this.total = 15
+    this.total = 15;
   }
   if (this.size === "16") {
-    this.total = 18
+    this.total = 18;
   }
   if (this.size === "20") {
-    this.total = 22
+    this.total = 22;
   };
-  this.total += this.toppings.length * 1.5
+  this.total += this.toppings.length * 1.5;
   return this.total;
 };
 
 
-Pizza.prototype.listToppings = function() {
-  for (var i = 0; i < this.toppings.length; i++) {
-    this.toppings[i].append("<li>" + i + "</li>")
-  }
+Pizza.prototype.showPizza = function(i){
+  var pizzaOutput = "";
+  console.log(this.toppings);
+  this.toppings.forEach(function(i){
+    pizzaOutput += "<li>" + [i] + "</li>";
+  })
+  return pizzaOutput;
 }
 
 /////////User Interface//////////
@@ -45,13 +48,14 @@ $(function() {
       };
     });
 
-    $("#topList").html(pizza.listToppings)
-    console.log(toppings);
-    pizza.size = $("input:radio[name=pizzaSize]:checked").val();
+    var topListOut = pizza.showPizza(pizza);
+    console.log(topListOut);
+    $("#topList").html(pizza.showPizza());
+    console.log();
+    pizza.size =  $("input:radio[name=pizzaSize]:checked").val();
     var pizSiz = pizza.size;
     $("span#sizeOut").text(pizSiz + "-inch pizza");
-    var price = pizza.findTotal();
+    var price = pizza.findTotal(pizza);
     $("span#priceOut").text("$" + price)
-
   });
 });
